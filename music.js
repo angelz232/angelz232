b95fc32b33bf7074f661dfc7905694b971d88ed1
angelz232 <im@trolli.ng>
2024-10-29 14:50:41 +0100

    Update music.js

--- music.js
+++ music.js
@@ -1,8 +1,8 @@
 const audioFiles = [
-    { src: "https://files.catbox.moe/fo805m.mp3", artist: "Ken Karson, Destroy Lonely", song: "Murda Musik" },
-    { src: "https://files.catbox.moe/vlohr2.mp3", artist: "22Gz", song: "Twirlanta" },
-    { src: "https://files.catbox.moe/ebvch3.mp3", artist: "Jdot Breezy", song: "Tweak Shit, Pt. 2" },
-    { src: "https://files.catbox.moe/xo4vuv.mp3", artist: "Jdot Breezy", song: "Shoot It Out" }, 
+//    { src: "https://files.catbox.moe/fo805m.mp3", artist: "Ken Karson, Destroy Lonely", song: "Murda Musik" }, 
+//    { src: "https://files.catbox.moe/vlohr2.mp3", artist: "22Gz", song: "Twirlanta" },
+//    { src: "https://files.catbox.moe/ebvch3.mp3", artist: "Jdot Breezy", song: "Tweak Shit, Pt. 2" },
+//    { src: "https://files.catbox.moe/xo4vuv.mp3", artist: "Jdot Breezy", song: "Shoot It Out" }, 
     { src: "https://files.catbox.moe/vqe2s4.mp3", artist: "Shoreline Mafia", song: "Bands" },
 ];
 
