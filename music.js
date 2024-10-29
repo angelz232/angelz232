d546429b4b2351b5f48046b79abab68ae1065044
angelz232 <im@trolli.ng>
2024-10-29 09:37:19 +0100

    Update music.js

--- music.js
+++ music.js
@@ -1,4 +1,3 @@
-
 const audioFiles = [
     { src: "https://files.catbox.moe/fo805m.mp3", artist: "Ken Karson, Destroy Lonely", song: "Murda Musik" },
     { src: "https://files.catbox.moe/vlohr2.mp3", artist: "22Gz", song: "Twirlanta" },
@@ -6,12 +5,10 @@ const audioFiles = [
     { src: "https://files.catbox.moe/xo4vuv.mp3", artist: "Jdot Breezy", song: "Shoot It Out" }
 ];
 
-
-let currentTrack = 0;
+let currentTrack = Math.floor(Math.random() * audioFiles.length);
 const music = new Audio(audioFiles[currentTrack].src);
 music.muted = false;  
 music.preload = "auto";  
-
 
 function toggleMusic() {
     if (music.paused) {
@@ -25,13 +22,17 @@ function toggleMusic() {
     }
 }
 
-
 document.body.addEventListener("click", toggleMusic);
 document.body.addEventListener("touchstart", toggleMusic);
 
-
 music.addEventListener("ended", () => {
-    currentTrack = (currentTrack + 1) % audioFiles.length;
+    // Select a random track different from the current one
+    let nextTrack;
+    do {
+        nextTrack = Math.floor(Math.random() * audioFiles.length);
+    } while (nextTrack === currentTrack);
+
+    currentTrack = nextTrack;
     music.src = audioFiles[currentTrack].src;
     music.play()
         .then(() => {
